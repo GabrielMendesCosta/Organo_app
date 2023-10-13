b0906290788912c29494700fe7387e8082ed9f4a
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-13 14:52:02 -0300

    feat: add the fields to the insert "cargo" and "img".
    Adicionamos os campos para escrever o "cargo" e o endereço da sua "imagem".

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto />
-      <CampoTexto />
-      <CampoTexto />
+      <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
     </div>
   );
 }
