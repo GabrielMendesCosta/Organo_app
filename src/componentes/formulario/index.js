0c23301d79b756c37a1fb9fcdd9d6e6777865798
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-16 12:37:49 -0300

    feat: add the "Time" field to a drop-down list.
    Foi adicionado uma lista suspensa para que seja selecionado o time.

--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../campoTexto'
+import ListaSuspensa from '../listaSuspensa'
 import './formulario.css'
 
 function Formulario(){
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return(
         <section className='formulario'>
             <form>
@@ -9,6 +21,7 @@ function Formulario(){
                 <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label ="Time" itens={times}/>
             </form>
         </section>
     )
---
+++ src/componentes/listaSuspensa/index.js
@@ -0,0 +1,16 @@
+import './listaSuspensa.css'
+
+function ListaSuspensa(props){
+
+    console.log(props.itens)
+    return(
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select>
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
