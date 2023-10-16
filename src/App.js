5cc8a36560739c211e2ce258bdd177db4a89dc47
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-16 12:10:57 -0300

    feat: add and customize the "formulario".
    Foi adicionado e customizado o menu de formulários para inserção de dados.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/banner';
-import CampoTexto from './componentes/campoTexto';
+import Formulario from './componentes/formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 }
