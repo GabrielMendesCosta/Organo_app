55d7fdf8fa2d1e1125ef69eee80419b16cba3638
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-18 13:42:16 -0300

    feat: add the card for collaborators.
    Foi criado o cartão dos colaboradores, de forma estática.

---
+++ src/componentes/colaborador/index.js
@@ -0,0 +1,15 @@
+import "./colaborador.css"
+
+function Colaborador(){
+    return(<div className="colaborador">
+        <div className="cabecalho">
+            <img src='https://github.com/GabrielMendesCosta.png' alt='Gabriel Mendes'/>
+        </div>
+        <div className="rodape">
+            <h4>Gabriel</h4>
+            <h5>DEV</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
--- src/componentes/time/index.js
+++ src/componentes/time/index.js
@@ -1,9 +1,11 @@
+import Colaborador from "../colaborador"
 import "./time.css"
 
 function Time(props){
     return(
         <section className="time" style={{backgroundColor:props.corSecundaria}}>
             <h3 style={{borderColor:props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador />
         </section>
     )
 }
