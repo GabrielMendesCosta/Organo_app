642c403c0307548507743fc427cbe85ef230b619
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-18 14:02:45 -0300

    feat: news collaborators only appear in yours teams.
    Agora os colaboradores adicionados aparecem apenas em seus times,
    não mais de forma estática.

--- src/componentes/colaborador/index.js
+++ src/componentes/colaborador/index.js
@@ -1,13 +1,13 @@
 import "./colaborador.css"
 
-function Colaborador(){
+function Colaborador(props){
     return(<div className="colaborador">
         <div className="cabecalho">
-            <img src='https://github.com/GabrielMendesCosta.png' alt='Gabriel Mendes'/>
+            <img src={props.imagem} alt={props.nome}/>
         </div>
         <div className="rodape">
-            <h4>Gabriel</h4>
-            <h5>DEV</h5>
+            <h4>{props.nome}</h4>
+            <h5>{props.cargo}</h5>
         </div>
     </div>)
 }
